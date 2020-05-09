57737a02935ae7a46fac722d26e4047a07ec4059
khaphan132 <64716505+khaphan132@users.noreply.github.com>
2020-05-09 11:03:27 +0700

    Update BigNumber.h
    Thêm hàm chia 2

--- console/BigNumber.h
+++ console/BigNumber.h
@@ -1,31 +1,27 @@
 #pragma once
 #include <iostream>
-#include <stdio.h>
 #include <string>
-
-
 using namespace std;
 
-//so trong he thap phan
 class BigNumber
 {
 private:
 	string m_num;
-	int m_size; // so chu so thap phan
 public:
 	BigNumber();
 	BigNumber(string);
 	BigNumber(const BigNumber& other);
 	~BigNumber();
 
+	void fixNum();
 	void print();
 	string getNum();
 	BigNumber operator + (const BigNumber& other);
 	void multi2();//nhan voi 2
+	void divide2();
 	void multiPow2(int m);//nhan voi 2 mu m
 
-	bool operator < (const BigNumber& other);
 	BigNumber& operator = (const BigNumber& other);
 	BigNumber& operator = (const int& x);//gan bang 1 so nguyen
-};
 
+};
